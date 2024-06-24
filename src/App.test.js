c08921bf7019104dbff1796bbf4cbe602fe4c0a2
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-06-24 18:42:50 +1200

    add a test in app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import HelloWorld from "./components/HelloWorld";
+import HelloWorld from "./components/helloworld";
 
 
 test('renders correctly', () => {
